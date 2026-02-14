cc19115e1315bc28a2293b15533065e4f32c1f0c
Anant Tripathi <gammadeveloper5114@gmail.com>
2026-02-15 02:03:14 +0530

    just separated .exe file

--- Variables and Datatypes/Boolean/270925.cpp
+++ Variables and Datatypes/Boolean/270925.cpp
@@ -1,19 +1,24 @@
-#include<iostream>
-int main(){
-bool red_light ;
-std::cout<<"is the light is red: ";
-std::cin>>red_light;
-bool green_light;
-std::cout<<"is the light is green: ";
-std::cin>> green_light;
-if(red_light == true){
-std::cout<<"Stop"<<std::endl;}
-else{
-std::cout<<"Go"<<std::endl;}
-return 0;
-if(green_light == true);{
-std::cout<<"GO"<<std::endl;}
-else{
-std::cout<<"Stop"<<std::endl;}
+#include <iostream>
 
+int main() {
+
+    bool red_light;
+    std::cout << "Is the light red (1/0): ";
+    std::cin >> red_light;
+
+    bool green_light;
+    std::cout << "Is the light green (1/0): ";
+    std::cin >> green_light;
+
+    if (red_light == true) {
+        std::cout << "Stop" << std::endl;
+    }
+    else if (green_light == true) {
+        std::cout << "Go" << std::endl;
+    }
+    else {
+        std::cout << "Invalid signal" << std::endl;
+    }
+
+    return 0;
 }
