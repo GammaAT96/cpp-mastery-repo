8b8344536e31d056df8c1edec19caf5dab49fb10
Anant Tripathi <gammadeveloper5114@gmail.com>
2026-05-16 21:50:15 +0530

    array 3

---
+++ Array/160526array3.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+    int high_scores [] {99, 85, 72, 100, 54};
+    
+   
+        std::cout<<"The array has : "<<std::size(high_scores)<<" element"<<std::endl;
+    
+}
