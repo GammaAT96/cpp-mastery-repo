4a108ccf64e26231dad991069dd36fefc05a37d4
Anant Tripathi <gammadeveloper5114@gmail.com>
2026-05-16 23:45:26 +0530

    array 5

---
+++ Array/160526array5.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(){
+    char secret_word []{"code"};
+    std::cout<<"secret_word: "<<secret_word<<std::endl;
+    std::cout<<"secret word size: "<<std::size(secret_word)<<std::endl;
+}
