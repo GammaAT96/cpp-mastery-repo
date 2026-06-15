984f8a1e226b14772b2c5d99a6455289b0600624
Anant Tripathi <gammadeveloper5114@gmail.com>
2026-06-15 11:20:21 +0530

    vector_1

---
+++ vector/q1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+vector<int> vec;
+vec.push_back(10);
+vec.push_back(20);
+vec.push_back(30);
+vec.push_back(40);
+vec.push_back(50);
+cout<<"vector: ";
+for(size_t i{};i<vec.size();i++){
+    
+    cout<<vec[i]<<" ";
+}
+
+}
